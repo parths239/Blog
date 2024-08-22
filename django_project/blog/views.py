a9f0df1171c0ff845720cd9cf21365371db2e8b7
Parth Sharma <parthsharma@csu.fullerton.edu>
2024-08-22 09:23:34 -0700

    created database models for posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Parth Sharma',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'Date_Posted': 'August 21, 2024'
-    },
-    {
-        'author': 'Morgan Freeman',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'Date_Posted': 'August 21, 2024'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
