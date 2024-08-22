02ba4bf6df51220d51d87c51d5b0323b8ae9e2ff
Parth Sharma <parthsharma@csu.fullerton.edu>
2024-08-21 21:04:45 -0700

    reorganized html files

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+posts = [
+    {
+        'author': 'Parth Sharma',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'Date_Posted': 'August 21, 2024'
+    },
+    {
+        'author': 'Morgan Freeman',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'Date_Posted': 'August 21, 2024'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1> Blog Home </h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1> Blog About </h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
